741bde83a5b1cfaa27dd96167db89ae827ab7e69
Alvaro Sevilla <alvaro@chainalysis.com>
2019-12-11 15:33:44 +0000

    remove manual count from day 8

--- 2019/8/main.py
+++ 2019/8/main.py
@@ -1,9 +1,6 @@
 def split(lst, size):
     return [lst[i:i+size] for i in range(0, len(lst), size)]
 
-def count(l, v):
-    return sum(map(lambda x: 1 if x == v else 0, l))
-
 def collapse(layers):
     return [next(filter(lambda v: v != 2, lay)) for lay in zip(*layers)]
 
@@ -14,9 +11,10 @@ lenx, leny = 25, 6
 data = [int(x) for x in open('./input.txt').read().strip('\n')]
 layers = split(data, lenx*leny)
 
+
 # Part 1
-best = min(layers, key=lambda l: count(l, 0))
-print(count(best, 1) * count(best, 2))
+best = min(layers, key=lambda l: l.count(0))
+print(best.count(1) * best.count(2))
 
 # Part 2
 img = split(collapse(layers), lenx)
