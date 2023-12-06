385a37e3d1edb714c399d4fe6d9e2dc611a34315
Alvaro <alvaro@chainalysis.com>
2023-12-06 13:09:25 +0100

    2023 day 6

---
+++ 2023/day6.py
@@ -0,0 +1,24 @@
+import re
+from math import prod, floor, ceil
+
+data = """
+Time:      7  15   30
+Distance:  9  40  200
+""".strip().splitlines()
+
+data = open('inputs/day6.txt').read().splitlines()
+
+def possible_ranges(race):
+    time, dist = race
+    d = (time**2 - 4*dist)**0.5 / 2
+    min = ceil(time/2 - d)
+    max = floor(time/2 + d)
+    return max - min + 1
+
+# part 1
+races = list(zip(*[map(int, re.findall(r'\d+', line)) for line in data]))
+print(prod(map(possible_ranges, races)))
+
+# part 2
+race = [int(line.split(":")[1].replace(" ", "")) for line in data]
+print(possible_ranges(race))
