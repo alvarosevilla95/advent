c64b47727b69b5ad641f4c4b5955e9d74b908cb8
Alvaro <alvaro@chainalysis.com>
2023-12-16 14:13:58 +0100

    add viz for day 16

--- 2023/common.py
+++ 2023/common.py
@@ -1,8 +1,25 @@
-import regex as re, sys, os, time
+import regex as re, sys, os, time, curses
 from math import prod, floor, ceil, lcm
+from random import randint
 from functools import reduce, cache
 from itertools import chain, combinations, takewhile
 from collections import defaultdict, deque
 from multiprocessing import Pool
 
+
 sys.setrecursionlimit(100000) # heh
+
+def draw_grid(screen, grid, rate, fdraw, *args):
+    if rate < 0 and randint(0, -rate-1) != 0: return
+    for i, l in enumerate(grid):
+        for j, _ in enumerate(l):
+            if i < curses.LINES and j < curses.COLS:
+                screen.addstr(i, j, fdraw(j, i, grid, *args))
+    screen.refresh()
+    if rate > 0: time.sleep(rate)
+
+def wrap_in_curses(f, *args):
+    def cf(*args):
+        curses.use_default_colors()
+        f(*args)
+    curses.wrapper(cf, *args)
--- 2023/day16.py
+++ 2023/day16.py
@@ -17,24 +17,32 @@ data = open('inputs/day16.txt').read().splitlines()
 
 dirs = { 'N': (0, -1), 'E': (1, 0), 'S': (0, 1), 'W': (-1, 0) }
 
-def scan_light(x, y, d):
-    queue, seen = deque([(x, y, d)]), defaultdict(set)
+def scan_light(x, y, d, draw=None):
+    queue, seen, i = deque([(x, y, d)]), defaultdict(set), 1
     while queue:
-        x, y, d = queue.pop()
-        while 0<=x<len(data) and 0<=y<len(data) and d not in seen[(x, y)]:
-            seen[(x, y)].add(d)
-            if data[y][x] == '\\': d = 'NESW'[3-'NESW'.index(d)]
-            if data[y][x] == '/': d = 'NESW'[1-'NESW'.index(d)]
-            if data[y][x] == '|' and d in 'EW': d = 'S'; queue += [(x, y, 'N')]
-            if data[y][x] == '-' and d in 'NS': d = 'W'; queue += [(x, y, 'E')]
-            x, y = x+dirs[d][0], y+dirs[d][1]
+        x, y, d = queue.popleft()
+        if not(0<=x<len(data) and 0<=y<len(data) and d not in seen[(x, y)]): continue
+        seen[(x, y)].add(d)
+        if data[y][x] == '\\': d = 'NESW'[3-'NESW'.index(d)]
+        if data[y][x] == '/': d = 'NESW'[1-'NESW'.index(d)]
+        if data[y][x] == '|' and d in 'EW': d = 'SN'
+        if data[y][x] == '-' and d in 'NS': d = 'WE'
+        queue += [(x+dirs[i][0], y+dirs[i][1], i) for i in d]
+        if draw and (i:= i-1) == 0: 
+            draw(data, seen)
+            i = len(queue)
     return seen
 
-def scan_all_directions(i):
-    return max(len(scan_light(i, 0, 'S')),
-               len(scan_light(i, len(data)-1, 'N')),
-               len(scan_light(0, i, 'E')),
-               len(scan_light(len(data)-1, i, 'W')))
+def scan_all_directions(i, draw=None):
+    return max(len(scan_light(i, 0, 'S', draw)),
+               len(scan_light(i, len(data)-1, 'N', draw)),
+               len(scan_light(0, i, 'E', draw)),
+               len(scan_light(len(data)-1, i, 'W', draw)))
+
+def viz(screen):
+    draw_char = lambda x, y, grid, seen: grid[y][x] if grid[y][x] != '.' else '#' if (x, y) in seen else ' '
+    draw = lambda grid, seen: draw_grid(screen, grid, 0.01, draw_char, seen)
+    max(scan_all_directions(i, draw) for i in range(len(data)))
 
 if __name__ == '__main__':
     # part 1
@@ -42,3 +50,5 @@ if __name__ == '__main__':
 
     # part 2
     print(max(Pool().starmap(scan_all_directions, ([i] for i in range(len(data))))))
+
+    # wrap_in_curses(viz)
