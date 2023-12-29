af88dba93520b9088165a11aac4f783d31a1ab9d
Alvaro <alvaro@chainalysis.com>
2023-12-29 17:03:22 +0100

    2019 refactor intcode

--- 2019/intcode.py
+++ 2019/intcode.py
@@ -11,6 +11,10 @@ class IntcodeVm:
         while True: yield int(input('input: '))
 
     @staticmethod
+    def stdin_ascii():
+        while True: yield from map(ord, input()+'\n')
+
+    @staticmethod
     def pipe_one(val):
         yield val
 
@@ -23,56 +27,54 @@ class IntcodeVm:
         self.memory = defaultdict(int, dict(enumerate(memory)))
         self.ip = 0
         self.r = 0
-
-    def get_mem(self, i, pos=False):
-        if pos: return self.get_mem(self.get_mem(i))
-        if i < 0: assert False
-        return self.memory[i] 
+        self.in_s = IntcodeVm.std_in()
 
     def set_mem(self, i, v):
         self.memory[i] = v
 
     def parse_instruction(self):
-        c = self.get_mem(self.ip) % 100
-        v1 = self.get_value_lazy(1, self.get_mem(self.ip) // 100   % 10)
-        v2 = self.get_value_lazy(2, self.get_mem(self.ip) // 1000  % 10)
-        v3 = self.get_value_lazy(3, self.get_mem(self.ip) // 10000 % 10)
+        c = self.memory[self.ip] % 100
+        v1 = self.get_value(1, self.memory[self.ip] // 100   % 10)
+        v2 = self.get_value(2, self.memory[self.ip] // 1000  % 10)
+        v3 = self.get_value(3, self.memory[self.ip] // 10000 % 10)
         return c, v1, v2, v3
+        # vs = [self.get_value(i, self.memory[self.ip] // 10**(i+2) % 10) for i in range(3)]
+        # return c, *vs
 
-    def get_value_lazy(self, off, f):
-        if f == 0: return lambda l=False: self.get_mem(self.ip+off, not l)
-        if f == 1: return lambda l=False: self.get_mem(self.ip+off) 
-        if f == 2: return lambda l=False: self.get_mem(self.get_mem(self.ip+off) + self.r) if not l else self.get_mem(self.ip+off) + self.r
-        return 0
+    def get_value(self, off, f):
+        if f == 1: return lambda _=False: self.memory[self.ip+off] 
+        mem = self.memory[self.ip+off] if f == 0 else self.memory[self.ip+off] + self.r
+        return lambda l=False: mem if l else self.memory[mem] 
+
+    def eval_one(self, in_s=None): 
+        if in_s: self.in_s = in_s
+        if (val := self.step()) is not None: return val
+        return None
 
     def eval(self, in_s=None): 
-        if in_s is None: in_s = IntcodeVm.std_in()
-        self.in_s = in_s
+        if in_s: self.in_s = in_s
         while self.ip >= 0:
-            val = self.step()
-            if val is not None: return val
+            if (val := self.step()) is not None: return val
         return None
 
     def run(self, in_s=None): 
-        if in_s is None: self.in_s = IntcodeVm.std_in()
-        self.in_s = in_s
+        if in_s: self.in_s = in_s
         while True: 
-            o = self.eval(in_s)
-            if o is None: break
+            if (o := self.eval(in_s)) is None: break
             yield o
 
     def step(self):
         c, v1, v2, v3 = self.parse_instruction()
         if c == 1: # add
-            self.set_mem(v3(True), v1() + v2())
+            self.set_mem(v3(1), v1() + v2())
             self.ip += 4
 
         elif c == 2: # mul
-            self.set_mem(v3(True), v1() * v2())
+            self.set_mem(v3(1), v1() * v2())
             self.ip += 4
 
         elif c == 3: # input
-            self.set_mem(v1(True), next(self.in_s))
+            self.set_mem(v1(1), next(self.in_s))
             self.ip += 2
 
         elif c == 4: # output
@@ -87,11 +89,11 @@ class IntcodeVm:
             self.ip = v2() if v1() == 0 else self.ip +3
 
         elif c == 7: # cmp_lt
-            self.set_mem(v3(True),  1 if v1() < v2() else 0)
+            self.set_mem(v3(1),  1 if v1() < v2() else 0)
             self.ip += 4
 
         elif c == 8: # jmp_eq
-            self.set_mem(v3(True),  1 if v1() == v2() else 0)
+            self.set_mem(v3(1),  1 if v1() == v2() else 0)
             self.ip += 4
 
         elif c == 9: # set_r
