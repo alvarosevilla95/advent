d429a808b050b160116bcd2ed66c8b94fc64ed45
Alvaro <alvaro@chainalysis.com>
2025-12-04 19:14:51 +0100

    day 4 with convolve2d

--- 2025/day04.py
+++ 2025/day04.py
@@ -1,3 +1,6 @@
+import numpy as np
+from scipy.signal import convolve2d
+
 data = """
 ..@@.@@@@.
 @@@.@.@.@@
@@ -13,20 +16,19 @@ data = """
 
 data = open('inputs/day4.txt').read()
 
-grid = [[1 if x == "@" else 0 for x in line] for line in data.splitlines()]
+grid = np.array([[1 if x == "@" else 0 for x in line] for line in data.splitlines()])
 
-xt = len(grid[0])
-yt = len(grid)
-
-def neighbors(grid, x, y):
-  return sum(grid[i][j] for i in range(x-1, x+2) for j in range(y-1, y+2) if 0 <= i < xt and 0 <= j < yt and (i, j) != (x, y))
+kernel = np.array([[1, 1, 1],
+                   [1, 0, 1],
+                   [1, 1, 1]])
 
 removed = 0
 prev = grid
 while True:
-    after = [[ 1 if prev[x][y] == 1 and neighbors(prev, x, y) >= 4 else 0 for x in range(xt)] for y in range(yt)]
-    new_removed = sum(prev[x][y] - after[x][y] for x in range(xt) for y in range(yt))
-    if removed == 0: print(new_removed) # part 1
+    neighbor_count = convolve2d(prev, kernel, mode='same', boundary='fill')
+    after = ((prev == 1) & (neighbor_count >= 4)).astype(int)
+    new_removed = np.sum(prev - after)
+    if removed == 0: print(new_removed)  # part 1
     if new_removed == 0: break
     removed += new_removed
     prev = after
